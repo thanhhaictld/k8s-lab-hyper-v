6a29e3d8ff23eca57cb945c80037d57c7a403d50
hai.do <thanhhaictld@gmail.com>
2026-08-22 15:23:06 +0700

    feat: enhance ArgoCD traffic exposure with TLS certificate generation and update gateway configuration

--- hyperv-kubespray-lab/clusters/argo/02_argo_cd_expose_traffic.ps1
+++ hyperv-kubespray-lab/clusters/argo/02_argo_cd_expose_traffic.ps1
@@ -1,3 +1,40 @@
+#/bin/pwsh
+
+
+& ./kubectl.ps1
+
+Write-Host "Exposing ArgoCD traffic through kgateway..."
+
+Write-Host "Creating self-signed certificate for ArgoCD...";
+New-Item -Path "./ssl" -ItemType Directory -Force | Out-Null
+if(Test-Path -Path "./ssl/tls.key" -ErrorAction SilentlyContinue) {
+    Write-Host "Certificate already exists. Skipping generation."
+} else {
+    Write-Host "Generating self-signed certificate..."
+    openssl req -x509 -nodes -days 365 `
+    -newkey rsa:2048 `
+    -keyout ssl/tls.key `
+    -out ssl/tls.crt `
+    -subj "/CN=argocd.local" `
+    -addext "subjectAltName=DNS:argocd.local"
+}
+
+Write-Host "Creating TLS secret in kgateway-system namespace..."
+$forceResetTls = Read-Host "Force reset TLS secret? (y/n)"
+# delete old secret if it exists
+if($forceResetTls -eq "y") {
+    Write-Host "Force reset TLS secret."
+    kubectl delete secret app-tls -n kgateway-system
+}
+
+if( -not (kubectl get secret app-tls -n kgateway-system -o jsonpath='{.metadata.name}' 2>$null)) {
+    Write-Host "TLS secret does not exist. Creating new secret."
+    kubectl create secret tls app-tls `
+    --cert=ssl/tls.crt `
+    --key=ssl/tls.key `
+    -n kgateway-system
+}
+
 kubectl apply -f ./system/public_gateway.yaml
 
 kubectl apply -f ./system/argocd_http_route.yaml
@@ -7,6 +44,7 @@ $ARGO_HOST = "argocd.local"
 
 Write-Host "Update your hosts file with the following entry:"
 Write-Host "$ARGO_IP $ARGO_HOST"
+
 if(Get-Content -Path "C:\Windows\System32\drivers\etc\hosts" | Select-String -Pattern "$ARGO_IP $ARGO_HOST") {
     Write-Host "Entry already exists in hosts file."
 } else {
@@ -16,4 +54,4 @@ if(Get-Content -Path "C:\Windows\System32\drivers\etc\hosts" | Select-String -Pa
 
 Write-Host "ARGO_IP: $ARGO_IP"
 Write-Host "ARGO_HOST: $ARGO_HOST"
-curl.exe http://$ARGO_IP/ -H "Host: $ARGO_HOST" -v
+curl.exe https://$ARGO_IP/ -H "Host: $ARGO_HOST" -v
