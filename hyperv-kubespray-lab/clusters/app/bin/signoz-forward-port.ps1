0bde310d2b7df79e0dd70a6a899a049eae3f8cb9
hai.do <thanhhaictld@gmail.com>
2026-08-29 20:27:17 +0700

    feat: add OpenTelemetry demo application with necessary configurations and scripts

---
+++ hyperv-kubespray-lab/clusters/app/bin/signoz-forward-port.ps1
@@ -0,0 +1,3 @@
+& ./kubectl.ps1
+
+kubectl port-forward -n o11y svc/signoz 8080:8080
--- hyperv-kubespray-lab/clusters/app/kubectl.ps1
+++ hyperv-kubespray-lab/clusters/app/kubectl.ps1
@@ -1,6 +1,2 @@
 Write-Host "Setup kubectl context" -ForegroundColor Green
 $env:KUBECONFIG="./.kubeconfig/kubeconfig"
-
-kubectl get nodes -o wide;
-
-kubectl get pods -A
